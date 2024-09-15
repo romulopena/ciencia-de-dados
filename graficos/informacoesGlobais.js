c6c5c3df5b44f308502edee1bb2feb50a1f21eea
PROF ROMULO <rjlpena@gmail.com>
2024-09-15 16:48:59 +0000

    Construindo Gráficos

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,11 +3,16 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais(){
     const res = await fetch (url)
     const dados = await res.json()
-    console.log(dados)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
+    //console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = 
-    `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> de pessoas e que aproximadamente <span>${pessoasConectadas}</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos}minutos conectadas.`
+    //console.log(paragrafo)
     
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
