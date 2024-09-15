7ae97f1ab65c80eb6f6e50021cf251eb5583a1fd
PROF ROMULO <rjlpena@gmail.com>
2024-09-15 17:54:41 +0000

    Padronização Visual

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,4 @@
+import { getCSS } from "./common.js"
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -9,15 +10,22 @@ async function quantidadeUsuarios() {
         {
             x: nomeDasRedes,
             y: quantidadeUsuarios,
-            type: 'bar'
+            type: 'bar',
+            marker:{
+                color: getCSS('--primary-color')
+            }
+
         }
     ]
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+    }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, layout)
 
 }
-
 quantidadeUsuarios()
