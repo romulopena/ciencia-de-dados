3363b74d6e7611464acbffa729a1848f4ad4e833
PROF ROMULO <rjlpena@gmail.com>
2024-11-21 08:51:34 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,11 +3,11 @@ const url = 'https://raw.githubusercontent.com/romulopena/ciencia-de-dados/refs/
 async function visualizarInformacoesGlobais(){
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasComAcesso = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
+    const pessoasEducacao = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio_dia_estudando)
     //const minutos = Math.round((dados.tempo_medio_dia_estudando - horas) * 100)
-    const pessoasEstudando = ((pessoasComAcesso / pessoasNoMundo) * 100).toFixed(2)
+    const pessoasEstudando = ((pessoasEducacao/ pessoasNoMundo) * 100).toFixed(2)
     //console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
